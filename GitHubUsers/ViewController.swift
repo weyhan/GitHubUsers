8a2ccfbcbc3c21ed90374fd3fa885c26cc56a017
weyhan <weyhan@motileware.com>
2022-12-15 20:59:47 +0800

    Initial Commit

---
+++ GitHubUsers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubUsers
+//
+//  Created by WeyHan Ng on 15/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
