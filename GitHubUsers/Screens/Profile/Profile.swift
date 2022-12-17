85fb6946c40d4adb9d8d03e8591392592bfbfe32
weyhan <weyhan@motileware.com>
2022-12-17 18:38:42 +0800

    Implement profile screen skeleton.

---
+++ GitHubUsers/Screens/Profile/Profile.swift
@@ -0,0 +1,111 @@
+//
+//  Profile.swift
+//  GitHubUsers
+//
+//  Created by WeyHan Ng on 16/12/2022.
+//
+
+import SwiftUI
+
+struct Profile: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Header()
+                Followers()
+                    .padding(.horizontal)
+                UserDetails()
+                    .padding()
+                NoteField()
+                Spacer()
+                    .navigationTitle("Profile")
+            }
+        }
+    }
+}
+
+struct Header: View {
+    @State private var backgroundColor: Color = .clear
+    @State private var profileImage = UIImage(named: "DefaultProfileImage")!
+
+    var body: some View {
+        ZStack {
+            backgroundColor
+                .ignoresSafeArea()
+            Image(uiImage: profileImage)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 120)
+        }
+        .frame(height: 150)
+        .onAppear { self.setBackgroundColor() }
+    }
+
+    private func setBackgroundColor() {
+        backgroundColor = Color(profileImage.averageColor ?? .clear)
+    }
+}
+
+struct Followers: View {
+    var body: some View {
+        HStack {
+            Text("followers: 99999")
+            Spacer()
+            Text("following: 99999")
+        }
+    }
+}
+
+struct UserDetails: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text("name: -")
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Text("company: -")
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Text("www.example.com")
+                .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(8)
+        .clipped()
+        .shadow(radius: 3)
+        .overlay(
+            RoundedRectangle(cornerRadius: 8)
+                .stroke(.gray, lineWidth: 0.5)
+        )
+    }
+}
+
+struct NoteField: View {
+    @State private var profileText = ""
+
+    var body: some View {
+        VStack {
+            VStack {
+                Text("Notes:")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                TextEditor(text: $profileText)
+                    .foregroundColor(.secondary)
+                    .lineLimit(5)
+                    .frame(height: 180)
+                    .cornerRadius(8)
+                    .shadow(radius: 3)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(.gray, lineWidth: 0.5)
+                    )
+                Button("Save") { }
+            }
+        }
+        .padding()
+    }
+}
+
+struct Profile_Previews: PreviewProvider {
+    static var previews: some View {
+        Profile()
+    }
+}
+
