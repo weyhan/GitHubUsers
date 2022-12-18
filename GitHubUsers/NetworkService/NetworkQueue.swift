726c566cfab7fd868c22841c387f5be7e2835c91
weyhan <weyhan@motileware.com>
2022-12-18 21:51:31 +0800

    Implement network queue.

---
+++ GitHubUsers/NetworkService/NetworkQueue.swift
@@ -0,0 +1,165 @@
+//
+//  NetworkQueue.swift
+//  GitHubUsers
+//
+//  Created by WeyHan Ng on 18/12/2022.
+//
+
+import Foundation
+
+// MARK: - NetworkQueue Supporting Types
+/// A type that can be enqueued in the NetworkQueue singleton object.
+protocol NetworkQueueable {
+    func resume()
+}
+
+/// Methods for informing the network queue of the operation status for the executing network job.
+protocol NetworkQueueDelegate {
+    func release()
+}
+
+// MARK: - NetworkQueue Class
+
+/// Singleton network queue.
+///
+/// Queued network jobs are executed sequently where only one job will be processed at any one time.
+final class NetworkQueue: NetworkQueueDelegate {
+    // Make NetworkQueue a Singleton object.
+    static let shared = NetworkQueue()
+    private init() { }
+
+    /// Queue state code
+    ///
+    /// Queue can be in the following state:
+    /// - `started`: Queue is processing queued network items.
+    /// - `stopped`: Queue is not processing queued network items.
+    private enum StateCode {
+        case started
+        case stopped
+    }
+
+    // MARK: - Private Properties
+
+    private var state = StateCode.stopped
+
+    private let dispatchQueue = DispatchQueue.global(qos: .utility)
+
+    private var queueSemaphore = DispatchSemaphore(value: 1)
+    private var stateSemaphore = DispatchSemaphore(value: 1)
+    private var executeSemaphore = DispatchSemaphore(value: 1)
+
+    private var queue = [NetworkQueueable]()
+
+    // MARK: - Public Properties
+
+    /// A Boolean value indicating whether the network queue is empty.
+    var isEmpty: Bool {
+        queueSemaphore.wait()
+        let result = queue.isEmpty
+        queueSemaphore.signal()
+        return result
+    }
+
+    /// The number of jobs enqueued in the network queue.
+    var count: Int {
+        queueSemaphore.wait()
+        let result = queue.count
+        queueSemaphore.signal()
+        return result
+    }
+
+    // MARK: - Public Interface
+
+    /// Append a NetworkQueueable object to the network queue.
+    ///
+    /// The enqueueing is semaphore guarded and is thread-safe.
+    func enqueue(networkJob: NetworkQueueable) {
+        dispatchQueue.async {
+            self.queueSemaphore.wait()
+            self.queue.append(networkJob)
+            self.queueSemaphore.signal()
+        }
+    }
+
+    /// Resumes the network queue, if it is suspended.
+    ///
+    /// The processing of the network queued jobs are semaphore guarded and thread-safe. Each task will be executed sequentially
+    /// where only one job at a time will be executed.
+    func resume() {
+        dispatchQueue.async { [unowned self] in
+            guard start() else { return }
+
+            while !isEmpty {
+                executeSemaphore.wait()
+                let item = dequeue()
+                item?.resume()
+            }
+
+            stop()
+        }
+    }
+
+    // MARK: NetworkQueueDelegate Conformance
+
+    /// Signals the release of the current network job.
+    ///
+    /// The `release()` method is called by the currently processing network job upon completion of it's network
+    /// operation. The call signals to the network queue that the next job can proceed if one is available in the queue.
+    ///
+    /// - Important: This method must be call from the network job object upon completion of the job once and only once.
+    func release() {
+        executeSemaphore.signal()
+    }
+
+    // MARK: - Private Internal Methods
+
+    /// Dequeue first job from the network queue and returns the dequeued job to the caller.
+    ///
+    /// This method is thread-safe.
+    /// - Returns: Returns `NetworkQueueable` if queue is not empty, otherwise nil is returned.
+    private func dequeue() -> NetworkQueueable? {
+        queueSemaphore.wait()
+        let item = queue.removeFirst()
+        queueSemaphore.signal()
+
+        return item
+    }
+
+    /// Change the queue state to started if queue is not started.
+    ///
+    /// Set the queue state to `.started` if the queue state is not already in the `.started` state.
+    /// This method is thread-safe.
+    /// - Important: The return `Boolean` is to safeguard that the queue is not started more than once. Further processing of the
+    /// queue at the caller side must cease if this method returns `false`.
+    ///
+    /// - Returns: Returns `true` if queue can start processing, otherwise returns `false`.
+    private func start() -> Bool {
+        guard !isEmpty else {
+            return false
+        }
+
+        stateSemaphore.wait()
+        guard state == .stopped else {
+            stateSemaphore.signal()
+            return false
+        }
+
+        state = .started
+        stateSemaphore.signal()
+
+        return true
+    }
+
+    /// Change the queue state to stopped
+    ///
+    /// Set the queue state to `.stopped`.
+    /// This method is thread-safe.
+    private func stop() {
+        stateSemaphore.wait()
+        if state == .started {
+            state = .stopped
+        }
+        stateSemaphore.signal()
+    }
+
+}
---
+++ GitHubUsersTests/NetworkQueueTests.swift
@@ -0,0 +1,107 @@
+//
+//  NetworkQueueTests.swift
+//  GitHubUsersTests
+//
+//  Created by WeyHan Ng on 18/12/2022.
+//
+
+import XCTest
+
+final class NetworkQueueTests: XCTestCase {
+
+    func testQueueEnqueueDequeue() throws {
+        let queue = NetworkQueue.shared
+        let numberOfJobs = 10
+
+        XCTAssertTrue(queue.isEmpty, "Network queue should be empty")
+
+        for _ in 1...numberOfJobs {
+            queue.enqueue(networkJob: TestNetworkNode())
+        }
+
+        // Wait for enqueue because NetworkQueue enqueues in a different thread.
+        sleep(UInt32(numberOfJobs/10))
+        XCTAssert(queue.count == numberOfJobs, "Network queue should have \(numberOfJobs) jobs")
+
+        queue.resume()
+
+        // The mock network job takes a randam amount of time between 0.5 to 1 seconds to
+        // completes it's task. Therefore `numberOfJobs` seconds for `numberOfJobs` task
+        // is more than sufficient wait time.
+        sleep(UInt32(numberOfJobs))
+        XCTAssertTrue(queue.isEmpty, "Network queue is not empty but should be")
+    }
+
+    func testSequence() throws {
+        let queue = NetworkQueue.shared
+        let counter = Counter(initialCount: 0, limit: 1)
+        let numberOfJobs = 10
+
+        for _ in 1...numberOfJobs {
+            queue.enqueue(networkJob: TestNetworkNode(counter: counter))
+        }
+
+        queue.resume()
+        sleep(UInt32(numberOfJobs))
+
+        // Counter object keep track if the at any time the counter is over the limit.
+        // The TestNetworkQueueNode will increment counter when it is resumed and decrement
+        // the counter when the mock network task is finished. Therefore, if the counter's
+        // isOverLimit flag is set to `true`, it means the queue have executed more than
+        // one job at a time.
+        XCTAssertFalse(counter.isOverLimit, "Network Queue job execution is not synchronous")
+    }
+
+    // MARK: - Mockup
+
+    // Keep count and if count ever went over the limit
+    class Counter {
+        private var semaphore = DispatchSemaphore(value: 1)
+        private var count = 0
+        private var limit = 1
+        var isOverLimit = false
+
+        init(initialCount: Int = 0, limit: Int = 1) {
+            self.count = initialCount
+            self.limit = limit
+        }
+
+        var isZero: Bool { count == 0 }
+
+        func increment() {
+            semaphore.wait()
+            count += 1
+            if count > limit { isOverLimit = true }
+            semaphore.signal()
+        }
+
+        func decrement() {
+            semaphore.wait()
+            count -= 1
+            semaphore.signal()
+        }
+    }
+
+    class TestNetworkNode: NetworkQueueable {
+        let uuid = UUID().uuidString
+        let waitTime: Double
+        let counter: Counter?
+
+        init(counter: Counter? = nil) {
+            waitTime = Double.random(in: 0.5...1.0)
+            self.counter = counter ?? nil
+        }
+
+        func resume() {
+            print("[\(uuid)]: Wait time: \(waitTime)")
+            counter?.increment()
+            DispatchQueue.global(qos: .utility).asyncAfter(deadline: .now() + waitTime) {
+                self.counter?.decrement()
+                NetworkQueue.shared.release()
+                print("[\(self.uuid)]: Done")
+            }
+            print("[\(uuid)]: Dispatched")
+        }
+    }
+
+}
